b0c476db3f392b8e011239a1cb071fcac4afdac0
Andrew Barry <andrewbarry220@gmail.com>
2021-10-18 16:22:06 -0400

    Added rules of fizz buzz

---
+++ WhiteboardChallenges/FizzBuzz.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WhiteboardChallenges
+{
+    //Fizz Buzz
+    //Write a program that prints every number from 0 to 100 to the console
+    //If a number is divisible by 3, print ‘fizz’ instead of the number
+    //If a number is divisible by 5, print ‘buzz’ instead of the number
+    //If a number is divisible by 3 and 5, print ‘fizzbuzz’ instead of the number
+    class FizzBuzz
+    {
+        //member variables(has a)
+
+
+        //constructor
+        public FizzBuzz()
+        {
+
+        }
+         
+        
+        //member methods(can do)
+
+
+    }
+}
